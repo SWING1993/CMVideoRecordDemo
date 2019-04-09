8b4d35450a8c553d3f1f52bd3088e3571331b19c
SWING1993 <songguohua10446627@gmail.com>
2019-04-09 09:33:50 +0800

    设置资源访问权限

--- CMVideoRecordDemo/ViewController.m
+++ CMVideoRecordDemo/ViewController.m
@@ -17,6 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.view addSubview:button];
+    [button setTitle:@"录制" forState:UIControlStateNormal];
+    button.frame = CGRectMake(CGRectGetWidth(self.view.frame)/2 - 25, CGRectGetHeight(self.view.frame)/2 - 25, 50, 50);
+    [button.titleLabel setFont:[UIFont systemFontOfSize:20]];
+    [button addTarget:self action:@selector(videoRecordAction) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)videoRecordAction {
+    
 }
 
 
